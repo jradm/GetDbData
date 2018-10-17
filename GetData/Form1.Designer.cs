f2a4b943594774e5d52cc6194d65c7a4229e2f53
JR <janradmacher@gmail.com>
2018-10-17 16:55:53 +0200

    Login

---
+++ GetData/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace GetData
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxDatabase = new System.Windows.Forms.TextBox();
+            this.labelDatabase = new System.Windows.Forms.Label();
+            this.labelConnection = new System.Windows.Forms.Label();
+            this.textBoxConnection = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(447, 51);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUser.TabIndex = 1;
+            this.textBoxUser.Text = "User";
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(391, 54);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(50, 13);
+            this.labelUser.TabIndex = 3;
+            this.labelUser.Text = "DB User:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(367, 16);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(74, 13);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "DB Password:";
+            // 
+            // textBoxDatabase
+            // 
+            this.textBoxDatabase.Location = new System.Drawing.Point(201, 51);
+            this.textBoxDatabase.Name = "textBoxDatabase";
+            this.textBoxDatabase.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDatabase.TabIndex = 5;
+            // 
+            // labelDatabase
+            // 
+            this.labelDatabase.AutoSize = true;
+            this.labelDatabase.Location = new System.Drawing.Point(127, 54);
+            this.labelDatabase.Name = "labelDatabase";
+            this.labelDatabase.Size = new System.Drawing.Size(56, 13);
+            this.labelDatabase.TabIndex = 6;
+            this.labelDatabase.Text = "Database:";
+            // 
+            // labelConnection
+            // 
+            this.labelConnection.AutoSize = true;
+            this.labelConnection.Location = new System.Drawing.Point(120, 19);
+            this.labelConnection.Name = "labelConnection";
+            this.labelConnection.Size = new System.Drawing.Size(64, 13);
+            this.labelConnection.TabIndex = 7;
+            this.labelConnection.Text = "Connection:";
+            // 
+            // textBoxConnection
+            // 
+            this.textBoxConnection.Location = new System.Drawing.Point(201, 13);
+            this.textBoxConnection.Name = "textBoxConnection";
+            this.textBoxConnection.Size = new System.Drawing.Size(100, 20);
+            this.textBoxConnection.TabIndex = 8;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(447, 16);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 9;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxConnection);
+            this.Controls.Add(this.labelConnection);
+            this.Controls.Add(this.labelDatabase);
+            this.Controls.Add(this.textBoxDatabase);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxDatabase;
+        private System.Windows.Forms.Label labelDatabase;
+        private System.Windows.Forms.Label labelConnection;
+        private System.Windows.Forms.TextBox textBoxConnection;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
---
+++ GetData/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetData
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection sqlConnection;
+
+            string connection = textBoxConnection.Text;
+            string database = textBoxDatabase.Text;
+            string user = textBoxUser.Text;
+            string password = textBoxPassword.Text;
+
+            //Develop DB
+            connectionString = @"Data Source=localhost;Initial Catalog=Application;User ID=dbuser;Password=dbpass";
+
+            //Production DB
+            //connectionString = @"Data Source="+ connection +";Initial Catalog="+ database +";User ID="+ user +";Password="+ password +"";
+
+            sqlConnection = new SqlConnection(connectionString);
+
+            
+            try
+            {
+                sqlConnection.Open();
+                MessageBox.Show("Connection Open!");
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "Select username, password from Application";
+
+
+            sqlConnection.Close();
+        }
+    }
+}
